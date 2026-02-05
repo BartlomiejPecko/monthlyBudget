033afb45ac0ed4a0a63b79b3680afcce0711e1e1
Bartłomiej Pecko <bartlomiejpecko@gmail.com>
2026-02-05 20:41:19 +0100

    init: Spring Boot project setup with base dependencies

---
+++ backend/src/main/java/com/monthlybudget/MonthlyBudgetApiApplication.java
@@ -0,0 +1,13 @@
+package com.monthlybudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyBudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyBudgetApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/monthlybudget/MonthlyBudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monthlybudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyBudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
