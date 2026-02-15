4cc7b33a2b0166c440042cf534b175d6bcb33586
Bartłomiej Pecko <bartlomiejpecko@gmail.com>
2026-02-15 18:41:27 +0100

    render

--- backend/src/main/java/com/monthlybudget/config/CorsConfig.java
+++ backend/src/main/java/com/monthlybudget/config/CorsConfig.java
@@ -15,7 +15,10 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:80", "http://localhost"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:[*]",
+                "https://*.onrender.com"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
